fbc700acd07b56e695e3e55f1eb8afa5266bd88a
Władysław Komorek <46459787+wlakom@users.noreply.github.com>
2024-12-19 09:49:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -60,151 +60,135 @@ app.controller('MapCtrl', ['$scope', '$http', '$aside', '$alert', '$modal', func
     };
     $scope.initMap();
 
-    $scope.search = function() {
-      $scope.busy = true;
-      $http({
-        method: 'GET',
-        dataType: 'json',
-        url: 'https://nominatim.openstreetmap.org/search?q=' + encodeURIComponent($scope.searchText) + '&format=json'
-      }).
-              success(function(data) {
-                $scope.busy = false;
-                console.log(data);
-                if (data.length > 0) {
-                  var item = data[0];
-                  $scope.map.setView([item.lat, item.lon]);
-                  if ($scope.map.getZoom() < 13)
-                    $scope.map.setZoom(13);
-                }
-              }).
-              error(function(data, status) {
-                $scope.busy = false;
-                alert(data);
-              });
-    };
+ $scope.initMap = function() {
+  $scope.map = L.map('map', {
+    center: [52, 19.5],
+    zoom: 6,
+    layers: [new L.tileLayer($scope.static.background, {
+      attributionControl: false,
+      maxZoom: 23,
+      maxNativeZoom: 19,
+      opacity: 0.8
+    }), $scope.graves.layer]
+  });
+  new L.Hash($scope.map);
+  $scope.map.attributionControl.setPrefix('');
+};
+$scope.initMap();
 
-    ////////////////////////////////////////////////////////////////// LOADING AND PARSING DATA ////
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    $scope.loadDataPopup = function() {
-      modal.show();
+$scope.search = function() {
+  $scope.busy = true;
+  $http({
+    method: 'GET',
+    url: 'https://nominatim.openstreetmap.org/search?q=' + encodeURIComponent($scope.searchText) + '&format=json'
+  }).then(function(response) {
+    $scope.busy = false;
+    const data = response.data;
+    console.log(data);
+    if (data.length > 0) {
+      const item = data[0];
+      $scope.map.setView([item.lat, item.lon], 13);
     }
-    /**
-     * Loading data from overpass API
-     * @returns {void}
-     */
-    $scope.loadData = function() {
-      var b = $scope.map.getBounds();
-      var bbox = '(' + b.getSouthEast().lat + ',' + b.getNorthWest().lng + ',' +
-              b.getNorthWest().lat + ',' + b.getSouthEast().lng + ')';
+  }).catch(function(error) {
+    $scope.busy = false;
+    alert('Error: ' + error.data);
+  });
+};
 
-      $scope.busy = true;
-      $http({
-        method: 'GET',
-        dataType: 'jsonp',
-        url: $scope.static.api + encodeURIComponent('[out:json];relation["type"="person"]' + bbox + ';>>;out;')
+$scope.searchGraves = function() {
+  const searchName = $('#searchName').val();
+  if (searchName) {
+    $scope.loadDataAdv(searchName);
+  } else {
+    alert('Please enter a name to search.');
+  }
+};
 
-      }).
-              success(function(data) {
-                $scope.busy = false;
-                $scope.parseData(data.elements);
-              }).
-              error(function(data, status) {
-                $scope.busy = false;
-                alert(data);
-              });
-    };
+$scope.loadDataPopup = function() {
+  modal.show();
+};
 
-    $scope.loadDataAdv = function(name) {
-      var b = $scope.map.getBounds();
-      var bbox = '(' + b.getSouthEast().lat + ',' + b.getNorthWest().lng + ',' +
-              b.getNorthWest().lat + ',' + b.getSouthEast().lng + ')';
+$scope.loadData = function() {
+  const b = $scope.map.getBounds();
+  const bbox = `(${b.getSouthEast().lat},${b.getNorthWest().lng},${b.getNorthWest().lat},${b.getSouthEast().lng})`;
 
-      $scope.busy = true;
-      const query = '[out:json];(rel[name~"'+ name +'",i][type="person"]' + bbox + ';rel[family_name~"'+ name +'",i][type="person"]' + bbox + ';);>>; out;';
-      console.log(query);
-      $http({
-        method: 'GET',
-        dataType: 'jsonp',
-        url: $scope.static.api + encodeURIComponent(query)
+  $scope.busy = true;
+  $http({
+    method: 'GET',
+    url: $scope.static.api + encodeURIComponent(`[out:json];relation["type"="person"]${bbox};>>;out;`)
+  }).then(function(response) {
+    $scope.busy = false;
+    $scope.parseData(response.data.elements);
+  }).catch(function(error) {
+    $scope.busy = false;
+    alert('Error: ' + error.data);
+  });
+};
 
-      }).
-              success(function(data) {
-                $scope.busy = false;
-                $scope.parseData(data.elements);
-              }).
-              error(function(data, status) {
-                $scope.busy = false;
-                alert(data);
-              });
-    };
+$scope.loadDataAdv = function(name) {
+  const b = $scope.map.getBounds();
+  const bbox = `(${b.getSouthEast().lat},${b.getNorthWest().lng},${b.getNorthWest().lat},${b.getSouthEast().lng})`;
+  const query = `[out:json];(rel[name~"${name}",i][type="person"]${bbox};rel[family_name~"${name}",i][type="person"]${bbox};);>>;out;`;
+  
+  console.log(query);
+  $scope.busy = true;
+  $http({
+    method: 'GET',
+    url: $scope.static.api + encodeURIComponent(query)
+  }).then(function(response) {
+    $scope.busy = false;
+    $scope.parseData(response.data.elements);
+  }).catch(function(error) {
+    $scope.busy = false;
+    alert('Error: ' + error.data);
+  });
+};
 
-    $scope.searchGraves = function(a, b,c) {
-      // FIXME: Why is model not working here?
-      $scope.loadDataAdv($('#searchName').val());
-    };
+$scope.parseData = function(data) {
+  const graves = {};
 
-    /**
-     * Parsing data to building object
-     * @param {Object} data input from Overpass API
-     * @returns {void}
-     */
-    $scope.parseData = function(data) {
-      var graves = {};
+  data.forEach(item => {
+    if (item.type === 'node') {
+      graves[item.id] = {
+        id: item.id,
+        type: 'node',
+        latlng: [item.lat, item.lon],
+        tags: item.tags,
+        people: []
+      };
+    } else if (item.type === 'way') {
+      const latlngs = item.nodes.map(k => graves[k].latlng);
+      graves[item.id] = {
+        id: item.id,
+        type: 'way',
+        latlngs: latlngs,
+        tags: item.tags,
+        people: []
+      };
+    }
+  });
 
-      for (var i in data) {
-        var item = data[i];
-        if (item.type === "node")
-          graves[item.id] = {
-            id: item.id,
-            type: "node",
-            latlng: [item.lat, item.lon],
-            tags: item.tags,
-            people: []
-          };
-      }
-
-      for (var i in data) {
-        var item = data[i];
-        //2. ways
-        if (item.type === "way") {
-          var latlngs = [];
-          for (var j = 0; j < item.nodes.length; j++) {
-            var k = item.nodes[j];
-            latlngs.push(graves[k].latlng);
-          }
-          graves[item.id] = {
-            id: item.id,
-            type: "way",
-            latlngs: latlngs,
-            tags: item.tags,
-            people: []
-          };
+  data.forEach(item => {
+    if (item.type === 'relation') {
+      const person = {
+        id: item.id,
+        name: item.tags.name,
+        tags: item.tags
+      };
+      item.members.forEach(member => {
+        if (member.role === 'tomb') {
+          graves[member.ref].people.push(person);
         }
-      }
+      });
+    }
+  });
 
-      for (var i in data) {
-        var item = data[i];
-        if (item.type === "relation") {
-          var person = {
-            id: item.id,
-            name: item.tags.name,
-            tags: item.tags,
-          };
-
-          for (var j in item.members) {
-            var member = item.members[j];
-            if (member.role === "tomb")
-              graves[member.ref].people.push(person);
-          }
-        }
-      }
-
-      $scope.graves.data = graves;
-      $scope.graves.layer.clearLayers();
-      for (var i in graves)
-        if (graves[i].tags)
-          $scope.drawGrave(i);
-    };
+  $scope.graves.data = graves;
+  $scope.graves.layer.clearLayers();
+  Object.values(graves).forEach(grave => {
+    if (grave.tags) $scope.drawGrave(grave.id);
+  });
+};
 
     /////////////////////////////////////////////////////////////////////////////////// DRAWING ////
     ////////////////////////////////////////////////////////////////////////////////////////////////
